ea07e59b3e17296d748b380f2d763497fa8c7328
automaticdai <automatic.dai@gmail.com>
2015-04-15 22:39:53 +0100

    V0.1.1 - April 15, 2015

--- ym4.cpp
+++ ym4.cpp
@@ -1,20 +1,30 @@
-/*
-	Arduino Library
-	for YM4 Mobile Robot Platform
+/* 
+	The Arduino Library
+	of YM4 Mobile Robot Platform
+	
 	YunFei Robotics Laboratory
-	http://www.yfworld.com
-	Version 0.1
-	March 22, 2015
+	http://www.yfworld.com/ym4
+	Version 0.1.1
+	April 15, 2015
 */
 
+#include "../MsTimer2/MsTimer2.h"
 #include <Arduino.h>
 #include "ym4.h"
 
-typedef void (*CALLBACK)(void);
+/* Global variables for inter-process communication */
+int gnLPulseCnt = 0;	// pulse count of the left motor, write by YM4
+int gnRPulseCnt = 0; 	// pulse count of the right motor, write by YM4
+int gnTick = 0;			// global time counts every 20ms, write by periodTask()
+
+bool gbLEDStatus = false;
 
 void YM4Class::init(void) {
-
-	// init Motor Control pin
+	
+	/* this statement changes the PWM output frequency to 7.8 KHZ */
+	TCCR2B = (TCCR2B & 0b11111000) | 0x02;
+	
+	/* init Motor Control pin */
 	pinMode(LCTRL1_PIN, OUTPUT);
 	pinMode(LCTRL2_PIN, OUTPUT);
 	pinMode(LCTRL3_PIN, OUTPUT);
@@ -23,17 +33,23 @@ void YM4Class::init(void) {
 	pinMode(RCTRL2_PIN, OUTPUT);
 	pinMode(RCTRL3_PIN, OUTPUT);
 
-	// init LED
+	/* init LED */
 	pinMode(LED_PIN, OUTPUT);
-  
-	// hook pulse sampling interrupt
-	//attachInterrupt(LPULSE_INT, (CALLBACK)&YM4Class::lPulseSample, FALLING);
-	//attachInterrupt(RPULSE_INT, (CALLBACK)&YM4Class::rPulseSapmle, FALLING);
+	bLEDStatus = false;
 	
+	/* hook the encoder pulse sampling interrupt */
+	attachInterrupt(LPULSE_INT, &YM4Class::lPulseSample, FALLING);
+	attachInterrupt(RPULSE_INT, &YM4Class::rPulseSapmle, FALLING);
+	
+	/* enable the timer interrupt */
+	MsTimer2::set(10, YM4Class::periodicHandle); // 10ms interrupt
+	MsTimer2::start();
 }
 
 
 void YM4Class::move(float speed) {
+	int nPWMVal;
+	
 	// trim the input
 	if (speed > 1) {
 		speed = 1;
@@ -43,87 +59,94 @@ void YM4Class::move(float speed) {
 		;
 	}
 	
+	// set direction
 	if (speed > 0) {
 		setMotorMode(M_LEFT, M_FORWARD);
 		setMotorMode(M_RIGHT, M_FORWARD);
 	} else if (speed < 0) {
 		setMotorMode(M_LEFT, M_BACKWARD);
-		setMotorMode(M_RIGHT, M_BACKWARD);		
+		setMotorMode(M_RIGHT, M_BACKWARD);
+		speed = -1 * speed;
 	}
 
-	int pmwVal;
-	
-	setMotorPWM(M_LEFT, 255);
-	setMotorPWM(M_RIGHT, 255);
+	// set speed
+	nPWMVal = (int)(speed * 255);
+	setMotorPWM(M_LEFT, nPWMVal);
+	setMotorPWM(M_RIGHT, nPWMVal);
 	
 }
 
 
 void YM4Class::turn(float speed) {
+	int nPWMVal;
+	
+	// trim the input
+	if (speed > 1) {
+		speed = 1;
+	} else if (speed < -1) {
+		speed = -1;
+	} else {
+		;
+	}
+	
+	nPWMVal = (int)(speed * 255);
+	
 	if (speed > 0) {
 		setMotorMode(M_LEFT, M_BACKWARD);
-		setMotorPWM(M_LEFT, 255);
 		setMotorMode(M_RIGHT, M_FORWARD);
-		setMotorPWM(M_RIGHT, 255);
+		setMotorPWM(M_LEFT, nPWMVal);
+		setMotorPWM(M_RIGHT, nPWMVal);
 	} else {
 		setMotorMode(M_LEFT, M_FORWARD);
-		setMotorPWM(M_LEFT, 255);
 		setMotorMode(M_RIGHT, M_BACKWARD);
-		setMotorPWM(M_RIGHT, 255);		
+		setMotorPWM(M_LEFT, nPWMVal);
+		setMotorPWM(M_RIGHT, nPWMVal);		
 	}
 }
 
 
 void YM4Class::setMotorMode(int motor, int mode) {
 
-  int Ctrl1,Ctrl2,Ctrl3;
-  
-  if (motor == M_LEFT) {
-    Ctrl1 = LCTRL1_PIN;
-    Ctrl2 = LCTRL2_PIN;
-    Ctrl3 = LCTRL3_PIN;
-  } else {
-    Ctrl1 = RCTRL1_PIN;
-    Ctrl2 = RCTRL2_PIN;
-    Ctrl3 = RCTRL3_PIN;
-  }
-  
-  switch (mode) {
-    case M_BACKWARD:
-    {
-      digitalWrite(Ctrl1, LOW);     // OFF H Bridge
-      digitalWrite(Ctrl2, HIGH);
-      digitalWrite(Ctrl3, LOW);
-      break;
-    }
+	int Ctrl1,Ctrl2,Ctrl3;
 
-    case M_FORWARD:
-    {
-      digitalWrite(Ctrl1, LOW);     // OFF H Bridge
-      digitalWrite(Ctrl2, LOW);
-      digitalWrite(Ctrl3, HIGH);
-      break;
-    }
-    
-    case M_BRAKE:
-    {
-      digitalWrite(Ctrl1, LOW);     // OFF H Bridge
-      digitalWrite(Ctrl2, LOW);
-      digitalWrite(Ctrl3, LOW);
-      break;
-    }
+	if (motor == M_LEFT) {
+		Ctrl1 = LCTRL1_PIN;
+		Ctrl2 = LCTRL2_PIN;
+		Ctrl3 = LCTRL3_PIN;
+	} else {
+		Ctrl1 = RCTRL1_PIN;
+		Ctrl2 = RCTRL2_PIN;
+		Ctrl3 = RCTRL3_PIN;
+	}
 
-    case M_FLOAT:
-    {
-      digitalWrite(Ctrl1, LOW);     // OFF H Bridge
-      digitalWrite(Ctrl2, HIGH);
-      digitalWrite(Ctrl3, HIGH);
-      break;
-    }
-    
-    default: 
-		;
-  } 
+	switch (mode) {
+		case M_BACKWARD:
+			digitalWrite(Ctrl1, LOW);     // OFF H Bridge
+			digitalWrite(Ctrl2, HIGH);
+			digitalWrite(Ctrl3, LOW);
+			break;
+
+		case M_FORWARD:
+			digitalWrite(Ctrl1, LOW);     // OFF H Bridge
+			digitalWrite(Ctrl2, LOW);
+			digitalWrite(Ctrl3, HIGH);
+			break;
+
+		case M_BRAKE:
+			digitalWrite(Ctrl1, LOW);     // OFF H Bridge
+			digitalWrite(Ctrl2, LOW);
+			digitalWrite(Ctrl3, LOW);
+			break;
+
+		case M_FLOAT:
+			digitalWrite(Ctrl1, LOW);     // OFF H Bridge
+			digitalWrite(Ctrl2, HIGH);
+			digitalWrite(Ctrl3, HIGH);
+			break;
+
+		default: 
+			;
+	} 
 }
 
 
@@ -136,10 +159,47 @@ void YM4Class::setMotorPWM(int motor, unsigned char ucPwmVal) {
 }
 
 
-void YM4Class::lPulseSample(void) {
-	;
+void YM4Class::led(int ledmode) {
+	switch (ledmode) {
+		case LED_OFF:
+			bLEDStatus = false;
+			digitalWrite(LED_PIN, HIGH);
+			break;
+			
+		case LED_ON:
+			bLEDStatus = true;
+			digitalWrite(LED_PIN, LOW);
+			break;
+		
+		case LED_FLASH:
+			bLEDStatus = !bLEDStatus;
+			bLEDStatus? digitalWrite(LED_PIN, LOW) : digitalWrite(LED_PIN, HIGH);
+			break;
+	
+	}
 }
 
+
+void YM4Class::lPulseSample(void) {
+	gnLPulseCnt++;
+}
+
+
 void YM4Class::rPulseSapmle(void) {
-	;
+	gnRPulseCnt++;
+}
+
+
+// this function will be executed every 10ms  
+void YM4Class::periodicHandle(void) {
+	
+	gnTick++;
+	
+	// flash the LED every 500ms
+ 	if (gnTick % 50 == 0) {
+		gbLEDStatus = !gbLEDStatus;
+		gbLEDStatus? digitalWrite(LED_PIN, LOW) : digitalWrite(LED_PIN, HIGH);
+	}
+	
+	
 }
--- ym4.h
+++ ym4.h
@@ -1,20 +1,21 @@
 #ifndef _YM4_H_INCLUDED
 #define _YM4_H_INCLUDED
 
-/* Digital Output */
-// Control pins for the left motor
+/* Pin Table */
+/* Control pins for the left motor */
 #define LCTRL1_PIN		(9)
 #define LCTRL2_PIN		(8)
 #define LCTRL3_PIN		(7)
 
-// Control pins for the right motor
+/* Control pins for the right motor */
 #define RCTRL1_PIN		(10)	// for the older pcb, this was Pin 6
 #define RCTRL2_PIN		(5)
 #define RCTRL3_PIN		(4)
 
+/* LED Pin */
 #define LED_PIN			(13)
 
-/* Analog Input */
+/* Analog Input Pins */
 #define LMOTORI_PIN		(6)
 #define LMOTORV_PIN		(1)
 
@@ -22,30 +23,41 @@
 #define RMOTORV_PIN		(0)
 
 /* Interrupt Number */
-#define LPULSE_INT 		(1)
-#define RPULSE_INT 		(0)
+#define LPULSE_INT 		(1)		// interrupt of the left-side speed encoder
+#define RPULSE_INT 		(0)		// interrupt of the right-side speed encoder
 
 /* Motor Modes */
+#define M_LEFT     		(0)
+#define M_RIGHT    		(1)
+
 #define M_FORWARD  		(1)
 #define M_BACKWARD 		(2)
 #define M_BRAKE    		(3)
 #define M_FLOAT    		(4)
 
-#define M_LEFT     		(0)
-#define M_RIGHT    		(1)
+/* LED Modes */
+#define LED_OFF			(0)
+#define LED_ON			(1)
+#define LED_FLASH		(2)
+
 
 class YM4Class {
 public:
 	YM4Class() {;}
 	void init(void);
-	void move(float speed);
-	void turn(float speed);
-  
+	void move(float speed);		// speed = [-1,1]
+	void turn(float speed);		// speed = [-1,1]
+	void led(int ledmode);
+	
+	static void lPulseSample(void);
+	static void rPulseSapmle(void);
+	static void periodicHandle(void);
+	
 private:
+	bool bLEDStatus;
 	void setMotorMode(int motor, int mode);
 	void setMotorPWM(int motor, unsigned char ucPwmVal);
-	void lPulseSample(void);
-	void rPulseSapmle(void);
+
 };
 
 #endif
